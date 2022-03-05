73969228ee4b8432842d1d74a6df72dc5e0329c3
Kate Kim <kay.k.projects@gmail.com>
2022-03-06 00:12:47 +1000

    github link updated

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="text-center">
-            <small>This project was coded by Kate Kim. Go to <a href="https://github.com/projectkatk/react-weather-search">Github</a></small>.  
+            <small>This project was coded by Kate Kim. Go to <a href="https://github.com/projectkatk/react-weather-search" target="_blank" rel="noreferrer">Github</a></small>.  
             <small className="d-block">API sources: <a href="https://www.weatherbit.io/api/">Weatherbit</a> & <a href="https://openweathermap.org/api">OpenWeather</a> </small>          
         </footer>
     )
